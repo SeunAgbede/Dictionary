0b11e1b353ca1bee76d63da8a2833047a47f6bd6
oluwaseun.agb <oluwaseun.agb@gmail.com>
2021-10-14 08:45:28 +0100

    Got the for loop working

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Seun");
+
+const searchBtn = document.querySelector('#searchBtn');
+const definition = document.querySelector('.definition');
+const word1 = document.querySelector('.word_1');
+const phonetic = document.querySelector('.phonetic');
+const origin = document.querySelector('.origin');
+
+
+searchBtn.addEventListener('click', findWord)
+
+async function findWord() {
+    const word = document.querySelector('.input').value; // collect word from user
+    const res = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word); // fetch the api with input word
+    const data = await res.json(); 
+
+    word1.innerHTML = data[0].word;
+    phonetic.innerHTML  = `[ ${data[0].phonetic} ]`;
+    origin.innerHTML = data[0].origin;
+    
+    document.querySelector('.definition').innerHTML = ''; // clears the document 
+    for (let i = 0; i < data[0].meanings.length ; i++) {
+        definition.innerHTML += `<p>${data[0].meanings[i].definitions[0].definition}</p>`;
+    }
+};
+
+
+
