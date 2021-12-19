b4c26713ddc9d84120ecd9b102427d87de83e31e
oluwaseun.agb <oluwaseun.agb@gmail.com>
2021-12-19 15:05:05 +0000

    added recursion to synonyms list

--- script.js
+++ script.js
@@ -1,86 +1,101 @@
-const searchBtn = document.querySelector('#searchBtn');
-const definition = document.querySelector('.definition');
-const word_1 = document.querySelector('.word_1');
-const phonetic = document.querySelector('.phonetic');
-const sound = document.querySelector('.sound');
-const origin = document.querySelector('.origin');
-const word2 = document.querySelector('#word');
-const audio = document.querySelector('.audio');
-const syn = document.querySelector('.syn');
-const syn_heading = document.querySelector('.syn_heading');
-const badWord = document.querySelector('.badWord');
-
+const searchBtn = document.querySelector("#searchBtn");
+const definition = document.querySelector(".definition");
+const word_1 = document.querySelector(".word_1");
+const phonetic = document.querySelector(".phonetic");
+const sound = document.querySelector(".sound");
+const origin = document.querySelector(".origin");
+const word2 = document.querySelector("#word");
+const audio = document.querySelector(".audio");
+const syn = document.querySelector(".syn");
+const syn_heading = document.querySelector(".syn_heading");
+const badWord = document.querySelector(".badWord");
 
 // Fires searchBtn when you press Enterkey in the input field
 word2.addEventListener("keyup", function (e) {
-    if (e.code === 'Enter') {
+    if (e.code === "Enter") {
         searchBtn.click();
     }
 });
 
-searchBtn.addEventListener('click', findWord)
-
+searchBtn.addEventListener("click", findWord);
 
 async function findWord() {
 
-    const word = document.querySelector('.input').value; // collect word from user
+    let word = document.querySelector(".input").value; // collect word from user
     const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`); // fetch the api with input word
     const data = await res.json();
-    
-    if (data.title == 'No Definitions Found') {  
-        
+
+    if (data.title == "No Definitions Found" || word == ' ') {
+
+
         badWord.innerHTML = `<p> Sorry, no definitions found for ${word} &#128533;</p>`;
 
-        badWord.classList.add('wobble-hor-top');
-        
-        // clears the document of any existing entries
-        sound.classList.remove('sound_1'); // removes the sound icon visibility
+        badWord.classList.add("wobble-hor-top");
 
-        syn_heading.classList.remove('syn_heading_1'); //removes the Synonymns heading
+        // clears the document of any existing entries
+        sound.classList.remove("sound_1"); // removes the sound icon visibility
+
+        syn_heading.classList.remove("syn_heading_1"); //removes the Synonymns heading
 
         const cls = document.querySelectorAll(".cls");
-        cls.forEach(function(e){
-            e.innerHTML = '';
+        cls.forEach(function (e) {
+            e.innerHTML = "";
         });
 
     } else {
         // Clears error message
-        document.querySelector('.badWord').innerHTML = '';
-        
-        badWord.classList.remove('wobble-hor-top');
+        document.querySelector(".badWord").innerHTML = "";
+
+        badWord.classList.remove("wobble-hor-top");
 
         word_1.innerHTML = data[0].word;
         phonetic.innerHTML = `[ ${data[0].phonetic} ]`;
 
-        // Add sound button to the DOM 
-        sound.classList.add('sound_1'); // makes the icon visible on click
-        
-        // Play word 
-        sound.addEventListener('click', function () {
+        // Add sound button to the DOM
+        sound.classList.add("sound_1"); // makes the icon visible on click
+
+        // Play word
+        sound.addEventListener("click", function () {
             audio.innerHTML = `<audio id="myAudio"> <source src="https://${data[0].phonetics[0].audio}"> </audio>`;
-            const myAudio = document.querySelector('#myAudio');
+            const myAudio = document.querySelector("#myAudio");
             myAudio.play();
         });
 
-        // Origin 
+        // Origin
         origin.innerHTML = `<p> origin <br> ${data[0].origin} </p>`;
 
-        // Definitions 
-        document.querySelector('.definition').innerHTML = ''; // clears the document 
+        // Definitions
+        document.querySelector(".definition").innerHTML = ""; // clears the document
         for (let i = 0; i < data[0].meanings.length; i++) {
             definition.innerHTML += `<p>${data[0].meanings[i].partOfSpeech}</p>
             <p>${data[0].meanings[i].definitions[0].definition}</p>
             <p>${data[0].meanings[i].definitions[0].example}</p>
-            <br> `
+            <br>`;
         };
 
-        // Synonyms 
-        document.querySelector('.syn').innerHTML = ''; // clears the document
-        syn_heading.classList.add('syn_heading_1'); //adds Synonymn heading
-       
-        let synNum = data[0].meanings[0].definitions[0].synonyms.slice(0, 7).length;
+        // Synonyms
+        document.querySelector(".syn").innerHTML = ""; // clears the document
+        syn_heading.classList.add("syn_heading_1"); //adds Synonymn heading
+        
+        let synNum = data[0].meanings[0].definitions[0].synonyms.slice(0, 7).length; //capping the number of synonyms
+        // loops through to display list of synonyms
         for (let i = 0; i < synNum; i++) {
-            syn.innerHTML += `<li>${data[0].meanings[0].definitions[0].synonyms[i]}</li>`;
+            syn.innerHTML += `<li><a class="synLists">${data[0].meanings[0].definitions[0].synonyms[i]}</a></li>`;
+
+            // Making the synonyms recursive 
+            let synLists = document.querySelectorAll('.synLists');
+
+            synLists.forEach( function (e) {
+
+                e.addEventListener('click', function (event) {
+                    // Converts synLists to an array and then outputs the index of clicked item to A 
+                    let A = Array.from(synLists).indexOf(event.target); 
+                    document.querySelector(".input").value = syn.innerText.split('\n')[A];
+
+                    findWord();
+                });
+            });
         };
-    }
+    };
 };
+
